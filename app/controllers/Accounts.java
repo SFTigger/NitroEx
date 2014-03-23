5b4fdcbd3bea0c1ba48a9dc9f4f2f051741a78b1
Adam Maass <adam@Winterfell.local>
2014-03-22 21:44:18 -0700

    add account create method in controller

--- app/controllers/Accounts.java
+++ app/controllers/Accounts.java
@@ -8,4 +8,10 @@ public class Accounts extends Controller {
         render();
     }
 
+    public static void create(String email, String password){
+    	Account a = new Account(email, password);
+    	a.save();
+    	
+    	// TOOD -- forward onto account view page
+    }
 }
