611c59e5ff3e6c01944ad4218763bfb4dc8db2a5
Adam Maass <adam.maass@comcast.net>
2014-03-23 06:27:55 -0700

    Add default security

--- app/controllers/Documents.java
+++ app/controllers/Documents.java
@@ -1,7 +1,10 @@
 package controllers;
 
 import play.mvc.Controller;
+import play.mvc.With;
+import play.mvc.Secure;
 
+@With(Secure.class)
 public class Documents extends Controller {
 
     public static void index() {
