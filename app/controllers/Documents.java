325556e320a99bf7436901d72e64a53160f39ea0
Adam Maass <adam.maass@comcast.net>
2014-03-29 16:08:42 -0700

    Extract security stuff

--- app/controllers/Documents.java
+++ app/controllers/Documents.java
@@ -3,13 +3,10 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.With;
 
-@With(Security.class)
 public class Documents extends Controller {
 
     public static void index() {
-    	String user = Security.connected();
-    	
-    	renderArgs.put("user", user);
+
         render();
     }
 }
